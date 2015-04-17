09c50826ba37c41543afa46dfb1cfe5dfd2527d7
Tariq <tariq.elahi@uwaterloo.ca>
2015-04-17 13:20:11 -0400

    updated the noise calculation.

--- S2/S2-netified/noise.py
+++ S2/S2-netified/noise.py
@@ -8,7 +8,6 @@ from privexUtils import resolution
 #def Noise(sensitivity, epsilon, delta, fingerprint, sigma):
 def Noise(sigma, fingerprint, sum_of_sq, p_exit):
     sigma_i = p_exit*sigma/math.sqrt(sum_of_sq) 
-
     random_sample = random.gauss(0,sigma_i)
     return random_sample 
 #    return 0
