78369134d7c884d6ed0bc95c9bc90e9225bb4bc8
Rob Jansen <jansen@cs.umn.edu>
2016-02-24 20:02:16 -0500

    new test script to check that tor exports privcount events

---
+++ test/test_tor_ctl_event.py
@@ -0,0 +1,20 @@
+from twisted.internet import reactor
+from twisted.internet.protocol import ReconnectingClientFactory
+from privcount.protocol import TorControlClientProtocol
+
+TOR_CONTROL_PORT=9050
+
+'''
+Connects to a local Tor Control port to test if events of type
+'PRIVCOUNT' are properly exported by Tor.
+'''
+
+class TorCtlClient(ReconnectingClientFactory):
+    def buildProtocol(self, addr):
+        return TorControlClientProtocol(self)
+    def handle_event(self, event):
+        print "got PRIVCOUNT event: " + event
+        return True
+
+reactor.connectTCP("127.0.0.1", TOR_CONTROL_PORT, TorCtlClient())
+reactor.run()
