2e558368ed437e788d976da1ff41d1fb80da46f1
teor <teor2345@gmail.com>
2016-12-01 16:26:59 +1100

    Add usage instructions to test_tor_ctl_event.py

--- test/test_tor_ctl_event.py
+++ test/test_tor_ctl_event.py
@@ -2,6 +2,14 @@ from twisted.internet import reactor
 from twisted.internet.protocol import ReconnectingClientFactory
 from privcount.protocol import TorControlClientProtocol
 
+# Usage:
+# cd privcount/tor
+# ./configure && make check && make test-network-all
+# src/or/tor PublishServerDescriptor 0 ControlPort 9051 ORPort 9001 DirPort 9030 ExitRelay 0 EnablePrivCount 1 DataDirectory `mktemp -d`
+# source venv/bin/activate
+# python test/test_tor_ctl_event.py
+# wait a few minutes for the first events to arrive
+
 # The typical control port listed in the tor manual
 TOR_CONTROL_PORT=9051
 
