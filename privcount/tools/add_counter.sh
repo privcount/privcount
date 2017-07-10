592b754bea84c16875a435c73fe4d8cde66bf826
teor <teor2345@gmail.com>
2017-07-10 17:46:21 +1000

    Make add_counters.sh add counters to doc/CounterTests.markdown
    Also make path handling more reliable.
    
    Closes #386.

--- privcount/tools/add_counter.sh
+++ privcount/tools/add_counter.sh
@@ -15,31 +15,37 @@ set -b
 
 BASE_DIR=${BASE_DIR:-`dirname "$0"`/../..}
 TEST_DIR=${TEST_DIR:-"$BASE_DIR"/test}
+DOC_DIR=${DOC_DIR:-"$BASE_DIR"/doc}
 
 # Clean up after last time
 rm "$BASE_DIR"/missing_counters || true
 
 echo "Finding missing counters:"
 # sort by reversed counter name, to sort similar suffixes together
-"$TEST_DIR"/test_counter_match.sh | grep "^+" | cut -d+ -f2 \
+"$TEST_DIR"/test_counter_match.sh | grep "^+" | cut -d+ -f2 | grep -v "^$" \
     | rev | sort -u | rev \
-    > missing_counters || echo "No missing counters." && exit 0
+    > "$BASE_DIR"/missing_counters && echo "No missing counters." && exit 0
+
+echo "Adding missing counters to CounterTests.markdown:"
+for counter in `cat "$BASE_DIR"/missing_counters`; do
+    echo "- $counter" >> "$DOC_DIR"/CounterTests.markdown
+done
 
 echo "Adding missing counters to counters.noise.yaml:"
-for counter in `cat missing_counters`; do
+for counter in `cat "$BASE_DIR"/missing_counters`; do
     echo "    $counter:" >> "$TEST_DIR"/counters.noise.yaml
     echo "        estimated_value: 0.0" >> "$TEST_DIR"/counters.noise.yaml
     echo "        sensitivity: 0.0" >> "$TEST_DIR"/counters.noise.yaml
 done
 
 echo "Adding missing counters to counters.sigmas.yaml:"
-for counter in `cat missing_counters`; do
+for counter in `cat "$BASE_DIR"/missing_counters`; do
     echo "    $counter:" >> "$TEST_DIR"/counters.sigmas.yaml
     echo "        sigma: 0.0" >> "$TEST_DIR"/counters.sigmas.yaml
 done
 
 echo "Adding missing counters to counters.bins.yaml:"
-for counter in `cat missing_counters`; do
+for counter in `cat "$BASE_DIR"/missing_counters`; do
      echo "    $counter:" >> "$TEST_DIR"/counters.bins.yaml
      echo "        bins:" >> "$TEST_DIR"/counters.bins.yaml
      echo "        - [-.inf, .inf]" >> "$TEST_DIR"/counters.bins.yaml
